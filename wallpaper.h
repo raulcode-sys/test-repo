4d455173b2adbff28f8a24dff7d6aa2118aa891e
Raul <matapecopac@gmail.com>
2026-05-28 00:20:09 +0100

    Add files via upload

---
+++ wallpaper.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "wallpaper_berserk.h"
+#include "wallpaper_anime.h"
+#include "wallpaper_napoleon.h"
+
+#define WP_W 1920
+#define WP_H 1080
+#define WP_COUNT 3
+
+typedef struct {
+    const char *name;
+    const unsigned int *data;
+} WPEntry;
+
+static const WPEntry wp_list[WP_COUNT] = {
+    {"Berserk",   wp_berserk_data},
+    {"Anime",     wp_anime_data},
+    {"Napoleon",  wp_napoleon_data},
+};
+
+static int wp_current = 0;
+
+#define wp_data (wp_list[wp_current].data)

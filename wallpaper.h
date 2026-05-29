95b42767d1431e7ec4bea8974c14f49ca49b1704
Raul <matapecopac@gmail.com>
2026-05-29 11:39:51 +0100

    Add files via upload

--- wallpaper.h
+++ wallpaper.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "wallpaper_berserk.h"
 #include "wallpaper_anime.h"
 #include "wallpaper_napoleon.h"
@@ -8,17 +7,12 @@
 #define WP_H 1080
 #define WP_COUNT 3
 
-typedef struct {
-    const char *name;
-    const unsigned int *data;
-} WPEntry;
-
+typedef struct { const char *name; const unsigned int *data; } WPEntry;
 static const WPEntry wp_list[WP_COUNT] = {
-    {"Berserk",   wp_berserk_data},
-    {"Anime",     wp_anime_data},
-    {"Napoleon",  wp_napoleon_data},
+    {"Berserk", wp_berserk_data},
+    {"Anime", wp_anime_data},
+    {"Napoleon", wp_napoleon_data},
 };
 
 static int wp_current = 0;
-
 #define wp_data (wp_list[wp_current].data)
